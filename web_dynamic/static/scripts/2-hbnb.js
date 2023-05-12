98a297dbe49d5afa12767edf627fcdafd6aa8485
Victor Emmanuel <victor.j.emmanuel@hotmail.com>
2023-05-12 14:05:12 +0000

    Revisiting These MOFOS

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,24 @@
+$('document').ready(function () {
+  const amenities = {};
+  $('INPUT[type="checkbox"]').change(function () {
+    if ($(this).is(':checked')) {
+      amenities[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else {
+      delete amenities[$(this).attr('data-id')];
+    }
+    $('.amenities H4').text(Object.values(amenities).join(', '));
+  });
+
+  $.ajax({
+    url: 'http://0.0.0.0:5001/api/v1/status/',
+    type: 'GET'
+  }).done(function (response) {
+    if (response.status === 'OK') {
+      $('#api_status').addClass('available');
+      console.log(response.status)
+    } else {
+      $('#api_status').removeClass('available');
+      console.log(response.status)
+    }
+  });
+});
